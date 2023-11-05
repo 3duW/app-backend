06618165616a1accd9c97c501c300c02a7b065eb
3duW <71583178@certus.edu.pe>
2023-11-05 13:10:26 -0500

    se realizo ejemplos de req y res

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("HELLO WORD");
+//importacion de paquetes
+const express = require("express")
+
+//definicion de configuraciones 
+const aplicacion = express()
+
+//ejecucion req(significa peticion) y res (respuestas que nosotros daremos )
+aplicacion.get("/prueba/express", function(req,res) {
+    res.send("hola,estas en la wed de prueba asdafasxc")
+})
+
+aplicacion.listen(4000)
